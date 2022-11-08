7dd21af0947d32defcc75828f6ce59b2c31fb44f
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-09 08:54:24 +0900

    feat: App.js 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Todo from './Pages/Todo';
 function App() {
-  return <div>안녕</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
