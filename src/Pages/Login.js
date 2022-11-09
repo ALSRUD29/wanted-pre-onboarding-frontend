21a8fadef133142e4cc7b09eef0c1094b3ec0af7
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-09 21:37:18 +0900

    assignment1 이메일, 비밀번호 유효성 검사기능 구현

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const disabled = !email.includes('@') || password.length < 8;
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">이메일</label>
-          <input type="email" htmlFor="email"></input>
+          <input
+            type="email"
+            htmlFor="email"
+            onChange={handleEmailChange}
+          ></input>
         </div>
         <div>
           <label htmlFor="password">비밀번호</label>
-          <input type="password" htmlFor="password"></input>
+          <input
+            type="password"
+            htmlFor="password"
+            onChange={handlePasswordChange}
+          ></input>
         </div>
-        <button>버튼</button>
+        <button disabled={disabled}>버튼</button>
       </form>
     </Container>
   );
