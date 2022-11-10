3660b5ecf2d6f2288f7b4844424114b9e2a850fb
ALSRUD29 <dev.mnkg@gmail.com>
2022-11-10 22:36:56 +0900

    feat: todolist 삭제기능 구현 완료

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,36 +1,46 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const Todo = () => {
+  const ref = useRef();
   const [todo, setTodo] = useState('');
   const [todos, setTodos] = useState([]);
 
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
-  const handleClick = () => {
+  const handleCreate = () => {
     if (todo === '') {
       return;
     }
     setTodos((current) => [todo, ...current]);
     setTodo('');
   };
+  const handleUpdate = () => {};
+  const handleRemove = (event) => {
+    setTodos(todos.filter((ele) => ele !== todos[event.target.value]));
+  };
   return (
     <Container>
+      <h2>TodoList</h2>
       <div>
         <input value={todo} onChange={handleChange}></input>
-        <button onClick={handleClick}>추가</button>
+        <button onClick={handleCreate}>추가</button>
       </div>
       <div>
         {todos.map((ele, index) => (
           <ListContainer key={index}>
             <div>
               <input type="checkbox"></input>
-              <span key={index}>{ele}</span>
+              <span key={index} ref={ref}>
+                {ele}
+              </span>
             </div>
             <div>
-              <button>수정</button>
-              <button>삭제</button>
+              <button onClick={handleUpdate}>수정</button>
+              <button onClick={handleRemove} value={index}>
+                삭제
+              </button>
             </div>
           </ListContainer>
         ))}
@@ -42,9 +52,11 @@ const Todo = () => {
 export default Todo;
 
 const Container = styled.div`
+  padding: 20px;
   > div {
     > input {
       width: 400px;
+      margin-bottom: 20px;
     }
   }
 `;
